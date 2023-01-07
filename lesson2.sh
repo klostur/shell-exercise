575c8ab04d879f50ccd2a8e97c0e428ea692b594
David Divijakov <klostur@pop-os.localdomain>
2023-01-07 18:25:21 +0100

    Added lesson two and .gitignore to stop tracking temporary Nano files

---
+++ lesson2.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Comments and Variables
+# Comments are written with a hashtag before
+echo "Hello Welt";
+echo $BASH;
+echo $HOME;
+echo $BASH_VERSION;
+echo $PWD;
+
+# Variables
+# For some reason, there's no reason to declare the variable, just assign a value to it's reference
+
+name=David
+echo $name;
