f3766198ff7f5e3fe9f502149f13a5988de63110
Klostur <david.divijakov@gmail.com>
2023-01-08 22:11:56 +0100

    added check for permision and appending data to file

--- lesson6.sh
+++ lesson6.sh
@@ -10,9 +10,17 @@
 
 echo -e "Enter the name of the file: \c" #-e flag enable interpretation of the backslash
 read file_name
+
 if [ -f $file_name ]
 then
   echo "$file_name found"
+  if [ -w $file_name ]
+  then
+    echo "Type some text. To quit, press ctrl+d"
+    cat >> $file_name 
+  else
+    echo "You do not have the permision to write"
+  fi
 else
   echo "$file_name NOT found"
 fi
