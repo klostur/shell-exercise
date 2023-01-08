7e0c9f93f54ae38d8d82a0a9deb9551af6824574
Klostur <david.divijakov@gmail.com>
2023-01-08 17:03:39 +0100

    if statments

---
+++ lesson5.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# If statements
+# Basic skeleton
+# if [ condition ]
+# then 
+#   statement
+# fi
+
+while :
+do
+read -p 'Write a number or press ctrl+c to stop: ' count
+
+if [ $count -gt 0 ]
+then
+  if [ $count -lt 10 ]
+  then
+    echo "The number you've entered is less than 10. We'll subtract one."
+    let count--
+    echo "Now your number is $count."
+  else
+    echo "It seems that your number is greater than 10."
+    echo "We'll add one, just for good measure."
+    let count++
+    echo "Now you have $count, to be precise."
+  fi
+else
+  echo "The current number is $count"
+fi
+echo "#############################"
+done
+
+
